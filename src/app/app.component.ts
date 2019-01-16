469fba587754c59ccdbb7ebd994016b9c259129c
Jessie J Valladares <shajessie@me.com>
2019-01-15 19:22:10 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'solux-landing';
-}
+export class AppComponent {}
+
+// To make apps from this:
+// 1: Change the component prefix to my own app
