fdecc55949ddaa6a06733f393f72a9e51a147839
Jessie J Valladares <shajessie@me.com>
2019-07-06 20:44:51 -0400

    Fixing unit tests for build

--- src/app/sandbox/sandbox-root/sandbox-root.component.spec.ts
+++ src/app/sandbox/sandbox-root/sandbox-root.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SandboxRootComponent } from './sandbox-root.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SandboxRootComponent', () => {
   let component: SandboxRootComponent;
@@ -8,7 +9,8 @@ describe('SandboxRootComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SandboxRootComponent ]
+      declarations: [ SandboxRootComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   }));
