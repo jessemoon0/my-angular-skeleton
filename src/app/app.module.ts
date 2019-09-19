cae89af2c7fd414d8b8dbd299586993e1df0465d
Jessie J Valladares <shajessie@me.com>
2019-09-19 02:24:08 -0400

    Small change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
--- src/app/auth/store-auth/selectors/auth.selectors.ts
+++ src/app/auth/store-auth/selectors/auth.selectors.ts
@@ -4,4 +4,3 @@ import { AuthState } from '../reducers/auth-state.reducer';
 export const selectAuthState = createFeatureSelector<AuthState>('authState');
 
 export const isLoggedIn = createSelector(selectAuthState, (authState) => authState.isLoggedIn);
-export const isLoggedOut = createSelector(isLoggedIn, (loggedIn) => !loggedIn);
