e56e05015ef6746e370a271b36f290a01879224d
Jessie J Valladares <shajessie@me.com>
2019-12-17 10:36:57 -0500

    Angular full update

--- src/app/sandbox/sandbox-root/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox-root/sandbox/sandbox.component.ts
@@ -1,30 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { AppState } from '../../../core/store-app/reducers';
 import { SandboxActivated } from '../../store-sandbox/actions/sandbox.actions';
 import { selectIsSandboxActive } from '../../store-sandbox/selectors/sandbox.selectors';
+import { take, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-sandbox',
   templateUrl: './sandbox.component.html',
   styleUrls: ['./sandbox.component.scss']
 })
-export class SandboxComponent implements OnInit {
+export class SandboxComponent implements OnInit, OnDestroy {
   
-  isActivated = false;
+  public isActivated = false;
+  
+  private destroy$: Subject<void> = new Subject<void>();
   
   constructor(private store: Store<AppState>) { }
   
   ngOnInit() {
     this.store.pipe(select(selectIsSandboxActive))
+      .pipe(takeUntil(this.destroy$))
       .subscribe(
-        (isActive) => console.log('Is Active: ', isActive)
+        (isActive: boolean) => console.log('Is Active from store: ', isActive)
       );
   }
   
-  testAction() {
+  public testAction() {
     this.isActivated = !this.isActivated;
     this.store.dispatch(new SandboxActivated({ isActive: this.isActivated }));
   }
-
+  
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+  
 }
