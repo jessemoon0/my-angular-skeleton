f0ffedd1ff4f568e859f86bf3395996ab71176d8
Jessie J Valladares <shajessie@me.com>
2020-01-05 02:05:57 -0500

    Some changes in comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,23 +16,22 @@ export class AppComponent implements OnInit {
   }
 }
 
-// *** Supports Safari, IE11, Edge, Chrome, Firefox ***
+// *** Should Support Safari, IE11, Edge, Chrome, Firefox ***
 
 // To MAKE APPS FROM THIS SKELETON:
 
 // 0: Rename project with WebStorm.
 // -> Right click on project file and rename project and directory
 // -> index.html rename <title>
-// -> angular.json rename all angular-skeleton to my project-name
+// -> angular.json rename all webtronics-pre-launch to my project-name
 // 1: Change the component prefixes to my own app
 // -> Change prefix in angular.json, also in src/tslint - component-selector,
-// then search everywhere for angular-skeleton and replace all with my project names
+// then search everywhere for webtronics-pre-launch and replace all with my project names
 // (search on angular.json file if I cant find any)
 // 2: Rename variables in material theme and prefixes for scss components
 // 3: Push to GIT: FOLLOW INSTRUCTIONS IN Angular Tutorial
 // 4: Use sandbox files to write other feature modules
-// 5: Add my branding in my css variables and in my colors
-// 6: Add Analytics (optional)
+// 5: Add Analytics (optional)
 
 // TODO: Learn and setup deployment
 // --> Upload to AWS and automate.
